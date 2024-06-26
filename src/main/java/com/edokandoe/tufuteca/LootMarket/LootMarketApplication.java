7104a6790e37615c6193088e3d94616b408cc0c4
Tufuteca <tufutecalive@gmail.com>
2024-06-26 23:00:54 +0700

    Initial commit

---
+++ src/main/java/com/edokandoe/tufuteca/LootMarket/LootMarketApplication.java
@@ -0,0 +1,13 @@
+package com.edokandoe.tufuteca.LootMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edokandoe/tufuteca/LootMarket/LootMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edokandoe.tufuteca.LootMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
