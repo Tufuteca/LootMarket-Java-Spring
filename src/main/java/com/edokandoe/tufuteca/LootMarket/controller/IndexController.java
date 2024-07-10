1ea5860865d8ceefb1417d7fd94e4a34c9458158
Tufuteca <tufutecalive@gmail.com>
2024-07-10 12:59:34 +0700

    Добавлено отображение страниц

--- src/main/java/com/edokandoe/tufuteca/LootMarket/controller/IndexController.java
+++ src/main/java/com/edokandoe/tufuteca/LootMarket/controller/IndexController.java
@@ -2,7 +2,18 @@ package com.edokandoe.tufuteca.LootMarket.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-}
+    
+        @GetMapping("/")
+        public String getIndexPage() {
+            return "index";
+        }
+
+        @GetMapping("/authorization")
+        public String getAuthPage() {
+            return "authorization";
+        }
+    }
